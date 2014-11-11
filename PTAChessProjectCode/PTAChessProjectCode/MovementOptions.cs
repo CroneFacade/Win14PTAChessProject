59825417074ef7dec0e44a69fbb7a6579be5283d
Therése Barmer <therese.barmer.win14@utb.hermods.se>
2014-11-11 14:23:19 +0100

    Added bools to check if a piece can move or strike.

--- PTAChessProjectCode/PTAChessProjectCode/MovementOptions.cs
+++ PTAChessProjectCode/PTAChessProjectCode/MovementOptions.cs
@@ -12,8 +12,11 @@ namespace PTAChessProjectCode
        public int WalkingLength = 0;
        public int PositionValue = 0;
        public int IDOfMyPiece = -1;
+       public bool CanMove { get; set; }
+       public bool CanStrike { get; set; }
+
        
-       public MovementOptions(int x, int y, int length)
+       public MovementOptions(int x, int y, int length, bool canMove, bool canStrike)
        {
            PositionX = x;
            PositionY = y;
