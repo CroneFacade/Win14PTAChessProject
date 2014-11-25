5c76ff45637de57fe755c1fc075287fecba3acf4
Andreas <andekv10@student.hh.se>
2014-11-25 13:30:51 +0100

    Cleaned up GameEngine class

--- PTAChessProjectCode/PTAChessProjectCode/GameEngine.cs
+++ PTAChessProjectCode/PTAChessProjectCode/GameEngine.cs
@@ -3,69 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using ChessGUI;
 
 
 namespace PTAChessProjectCode
 {
     /// <summary>
-    /// Class handles the logic of the game
+    /// Handles all the logic of the chessgame
     /// </summary>
     public class GameEngine
     {
 
-
-
-
-        //Print Printer;
         public int countMoves = 1;
         public PlayerPieces AIWhiteComp;
         public PlayerPieces AIBlackComp;
         public AIMoveData moveData;
 
-
-
-        public GameEngine()
-        {
-            //printGui.PrintBoard(8);
-            //printerGui = new PrintGUI();
-            //Printer = new Print();
-            //AIWhiteComp = new AI();
-            //gui = new GUI();
-
-        }
         public void Start()
         {
-            //countMoves = 1;
-
             StartGame();
             Logger.CreateCleanLog();
         }
 
-        // This method controls the initiatiation of the game
         public void InitiateGame()
         {
             CreateAIs();
-            //Printer.PrintBoard(countMoves);
-            //printerGui.PrintBoard(countMoves);
-
-            //printerGui.PrintPieceOnBoard(AIWhiteComp.PieceList);
-            //printerGui.PrintPieceOnBoard(AIBlackComp.PieceList);
-
-            //Printer.PrintPieceOnBoard(AIWhiteComp.PieceList);
-            //Printer.PrintPieceOnBoard(AIBlackComp.PieceList);
             SetAITurn();
             var playerToBegin = FetchAIToBegin();
             var playerNotMovin = FetchAINotMoving();
             CreateMoveLogic(playerToBegin, playerNotMovin);
-
         }
 
-        public void CreateMoveLogic(PlayerPieces playerToBegin, PlayerPieces playerNotMovin)
+        /* ***** Initiate new game ******* */
+
+        public void CreateAIs()
         {
-            moveData = new AIMoveData(playerToBegin, playerNotMovin);
+            bool playerWhite = true;
+            bool playerBlack = false;
+            bool myTurn = false;
 
+            AIWhiteComp = new PlayerPieces(playerWhite, myTurn);
+            AIBlackComp = new PlayerPieces(playerBlack, myTurn);
         }
+
+        public void SetAITurn()
+        {
+            AIWhiteComp.MyTurn = true;
+        }
+
 
         public PlayerPieces FetchAIToBegin()
         {
@@ -91,35 +75,24 @@ namespace PTAChessProjectCode
             }
         }
 
-        public void CreateAIs()
+        public void CreateMoveLogic(PlayerPieces playerToBegin, PlayerPieces playerNotMovin)
         {
-            bool playerWhite = true;
-            bool playerBlack = false;
-            bool myTurn = false;
-
-            AIWhiteComp = new PlayerPieces(playerWhite, myTurn);
-            AIBlackComp = new PlayerPieces(playerBlack, myTurn);
+            moveData = new AIMoveData(playerToBegin, playerNotMovin);
 
         }
 
-        public void SetAITurn()
-        {
-            AIWhiteComp.MyTurn = true;
-        }
+        /* ####### iniate new game end ####### */
 
         public void StartGame()
         {
-
             bool continuePlaying = true;
 
             while (continuePlaying)
             {
-                // UpdateBoard();
                 Console.ReadLine();
                 continuePlaying = InitiateWhiteTurn(continuePlaying);
                 Console.ReadLine();
                 continuePlaying = InitiateBlackTurn(continuePlaying);
-
             }
         }
 
@@ -147,31 +120,13 @@ namespace PTAChessProjectCode
         {
             AIBlackComp.PieceList = moveData.MakeMove(AIBlackComp, AIWhiteComp.PieceList);
             countMoves++;
-            //  UpdateBoard();
         }
 
         private void WhiteMove()
         {
             AIWhiteComp.PieceList = moveData.MakeMove(AIWhiteComp, AIBlackComp.PieceList);
-            //countMoves++;
-            //   UpdateBoard();
             Console.ForegroundColor = ConsoleColor.Yellow;
         }
-
-        /*private void UpdateBoard()
-        {
-            Console.ForegroundColor = ConsoleColor.White;
-            Printer.PrintBoard(countMoves);
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Printer.PrintPieceOnBoard(AIWhiteComp.PieceList);
-            Console.ForegroundColor = ConsoleColor.Red;
-            Printer.PrintPieceOnBoard(AIBlackComp.PieceList);
-        }*/
-
-
-
-        //Game Over Menu Method
-
 
         public PlayerPieces CheckAITurn()
         {
@@ -197,31 +152,7 @@ namespace PTAChessProjectCode
                 AIBlackComp.MyTurn = true;
                 AIWhiteComp.MyTurn = false;
             }
-
-            /*if (AIWhiteComp.MyTurn == true)
-            {
-                AIWhiteComp.MyTurn = false;
-                AIBlackComp.MyTurn = true;
-                
-            }
-            else if (AIWhiteComp.MyTurn == false)
-            {
-                AIBlackComp.MyTurn = false;
-                AIBlackComp.MyTurn = true;
-                
-            }*/
         }
-
-
-        /* ********** INITIATE GAME BELOW ********************** */
-
-
-
-
-        /* ################### INITATE GAME END ########################## */
-
-        /* ********************** START GAME BELOW ************************** */
-
 
         private bool MatchCoordinate(int x, int y, int newX, int newY)
         {
